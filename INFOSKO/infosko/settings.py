d4777fa5c2d58b08d43b0f6a728609577c3e865c
vincivinicii <vincemicko2000>
2024-07-20 08:58:34 +0800

    Deploy

--- INFOSKO/infosko/settings.py
+++ INFOSKO/infosko/settings.py
@@ -28,7 +28,8 @@ SECRET_KEY = 'django-insecure-pls+37t)#8+y605=x$-1zaf+2ro$@xwx4v#kq4#1du#&m#fva4
 DEBUG = False
 
 ALLOWED_HOSTS = [
-    '143.198.192.156'
+    '143.198.192.156',
+    '127.0.0.1',
    'infosko.blog',
    'wwww.infosko.blog',
 ]
