262e8473b861848023516a48d96c196315a3295f
vincivinicii <vincemicko2000>
2025-01-17 13:48:47 +0800

    DEPLOY

--- infosko/settings.py
+++ infosko/settings.py
@@ -140,6 +140,9 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+# Enable Gzip and file compression (optional, but recommended)
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
