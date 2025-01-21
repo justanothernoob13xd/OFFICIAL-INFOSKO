4ed2c4a4502076c6ca80b55a7887666478258c5d
vincivinicii <vincemicko2000>
2025-01-21 18:29:27 +0800

    TRY AGAIN

--- INFOSKOProject/INFOSKOProject/main/pagination.py
+++
@@ -1,5 +0,0 @@
-from rest_framework.pagination import PageNumberPagination
-
-class PersonnelPagination(PageNumberPagination):
-    page_size = 21  # Adjust this number to load 7 rows per load (7 * 3 categories)
-    max_page_size = 50
