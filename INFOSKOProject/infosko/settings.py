42a7a110133b564755abf4bef192ea6f00bbe662
vincivinicii <vincemicko2000>
2025-01-25 13:22:31 +0800

    ADMIN

--- INFOSKOProject/infosko/settings.py
+++ INFOSKOProject/infosko/settings.py
@@ -167,7 +167,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-DEBUG = False
+DEBUG = True
 
 #CELERY
 # Redis settings
